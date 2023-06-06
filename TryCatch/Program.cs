be76d2478791a779984ca9723c29b8c896def5d4
Paulo <paulo.souza@estudante.ifro.edu.br>
2023-06-06 14:03:15 -0400

    Primeiro Commit

---
+++ TryCatch/Program.cs
@@ -0,0 +1,16 @@
+﻿
+
+
+try
+{
+    Console.WriteLine("informe o primeiro valor!");
+    int a = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("Informe o segundo valor!");
+    int b = Convert.ToInt32(Console.ReadLine());
+    int r = a / b;
+    Console.WriteLine($"O resultado é {r}");
+}catch(Exception ex)
+{
+    Console.WriteLine($"Não é permitido divisão por zero! { ex.Message}");
+} 
